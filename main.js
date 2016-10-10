9a027b70d15c5784ce26a7cc7b374eaf542460e9
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-10 14:50:44 -0400

    adding progress to work

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// Question 1
+
+var getPrices = function(item){
+	return item.price;
+}
+
+var answer1 = document.getElementById("answer1");
+var price = items.map(getPrices)
+
+var averagePrice = function (avgPriceArr){
+var total = 0;
+for(var i = 0; i < avgPriceArr.length; i++) {
+    total += avgPriceArr[i];
+    // total = total + avgPriceArr[i]; <-- Same as +=
+}
+	//return total / avgPriceArr.length;
+	var ans = total / avgPriceArr.length;
+
+	answer1.innerHTML = ans;
+};
+
+averagePrice(price);
+
+
+
+// Question 2
+
+
+
+var priceRange = items.filter(function (x) { 
+	return (x.price > 14 && x.price < 18)
+} )
+ 
+
+var answer2 = document.getElementById("answer2");
+
+answer2.innerHTML += priceRange[0].title;
+answer2.innerHTML += priceRange[1].title;
+answer2.innerHTML += priceRange[2].title;
+
+// Question 3
+
